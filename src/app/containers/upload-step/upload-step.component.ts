33443c38bcf7989e2272826a1fda091268ca09ec
dale wesdorp <dalewesdorp@gmail.com>
2022-01-27 12:07:56 +0100

    Upload!

--- src/app/containers/upload-step/upload-step.component.ts
+++ src/app/containers/upload-step/upload-step.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
+
+type AOA = any[][];
 
 @Component({
   selector: 'app-upload-step',
@@ -12,4 +15,28 @@ export class UploadStepComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  data: AOA = [ [1, 2], [3, 4] ];
+	wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+	fileName: string = 'SheetJS.xlsx';
+	ver: string = XLSX.version;
+
+	onFileChange(evt: any) {
+		/* wire up file reader */
+		const target: DataTransfer = <DataTransfer>(evt.target);
+		if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+		const reader: FileReader = new FileReader();
+		reader.onload = (e: any) => {
+			/* read workbook */
+			const ab: ArrayBuffer = e.target.result;
+			const wb: XLSX.WorkBook = XLSX.read(ab);
+
+			/* grab first sheet */
+			const wsname: string = wb.SheetNames[0];
+			const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+			/* save data */
+			this.data = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1}));
+		};
+		reader.readAsArrayBuffer(target.files[0]);
+	}
 }
