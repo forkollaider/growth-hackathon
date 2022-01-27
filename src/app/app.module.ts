25af9ea52340ad6efa2ee706f87512d1d3734cd0
Tikhon <tikhon.scherbakov@gmail.com>
2022-01-27 11:34:45 +0100

    add initial step components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UploadStepComponent } from './containers/upload-step/upload-step.component';
+import { StructureStepComponent } from './containers/structure-step/structure-step.component';
+import { PreviewStepComponent } from './containers/preview-step/preview-step.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadStepComponent,
+    StructureStepComponent,
+    PreviewStepComponent
   ],
   imports: [
     BrowserModule
